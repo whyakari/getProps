b92f9a513436c16dd0cd35318ebcb2496346c9bd
akari <akariondev@gmail.com>
2023-12-02 17:33:18 +0000

    update
    Signed-off-by: akari <akariondev@gmail.com>

--- gen_custom_pif.sh
+++ gen_custom_pif.sh
@@ -18,8 +18,6 @@ esac;
 shdir=$(dirname "$(readlink -f "$shdir")");
 
 cd $shdir;
-# cd "${GITHUB_WORKSPACE}/result" || die "Failed to change directory to ${GITHUB_WORKSPACE}/result";
-
 
 case $1 in
   json|prop) FORMAT=$1;;
@@ -57,13 +55,6 @@ esac;
 [ -z "$MODEL" ] && MODEL=$(file_getprop build.prop ro.product.product.model);
 [ -z "$FINGERPRINT" ] && FINGERPRINT=$(file_getprop build.prop ro.product.build.fingerprint);
 
-if [ -z "$FINGERPRINT" ]; then
-  if [ -f result/build.prop ]; then
-    die "No fingerprint found, use a /system/build.prop to start";
-  else
-    die "No fingerprint found, unable to continue";
-  fi;
-fi;
 echo "$FINGERPRINT";
 
 LIST="PRODUCT DEVICE MANUFACTURER BRAND MODEL FINGERPRINT";
