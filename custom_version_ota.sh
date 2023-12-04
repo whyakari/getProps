56f53dfbd5b19f86934e36d9a2723c4e5ac89377
明 <akariondev@gmail.com>
2023-12-03 22:08:47 -0300

    Update custom_version_ota.sh

--- custom_version_ota.sh
+++ custom_version_ota.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Using util_functions.sh
 [ -f "util_functions.sh" ] && . ./util_functions.sh || { echo "util_functions.sh not found" && exit 1; }
 
 if [ -z "$1" ]; then
@@ -12,7 +11,7 @@ android_version="tq3a" # e.g (Android 13)
 
 print_message "Started downloading Android $android_version OTAs for: ($*)" info
 
-for device_name in "${@}"; do # Permitir vários argumentos
+for device_name in "${@}"; do
   last_build_url=$(curl -Ls 'https://developers.google.cn/android/ota?partial=1' | grep -Eo "\"(\S+$device_name\S{33})?zip" | grep "$android_version" | tail -1 | tr -d \")
   print_message "Downloading ($device_name) \"$last_build_url\"..." debug
   wget --tries=inf --show-progress -q "$last_build_url"
