8509fbb3f49c1973fcca0826ea61e9e76f0d337b
akari <akariondev@gmail.com>
2023-12-06 03:32:49 +0000

    add extract image custom
    Signed-off-by: akari <akariondev@gmail.com>

---
+++ extract_images_custom.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+zip_file=$(find . -maxdepth 1 -type f -name "*.zip" -print -quit)
+
+if [ -z "$zip_file" ]; then
+    echo "Nenhum arquivo .zip encontrado no diretório atual."
+    exit 1
+fi
+
+extracted_dir="extracted_images"
+
+mkdir -p "$extracted_dir"
+
+unzip -o "$zip_file" -d "$extracted_dir" &>/dev/null
+
+for file in "$extracted_dir"/*; do
+    if [ -f "$file" ]; then
+        filename="${file##*/}"
+        basename="${filename%.*}"
+        mv -f "$file" "$extracted_dir/$basename.bin"
+    fi
+done
+
+echo "Extração concluída. Arquivos movidos para: $extracted_dir"
+
