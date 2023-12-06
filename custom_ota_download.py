b6323a32a485a80a3222fd37557125c5a295cbf5
akari <akariondev@gmail.com>
2023-12-06 01:37:25 +0000

    add args to pass
    Signed-off-by: akari <akariondev@gmail.com>

--- custom_ota_download.py
+++ custom_ota_download.py
@@ -1,29 +1,25 @@
+import sys
+from os import system
 from requests import get
 
 def get_zenfone():
     r = get("http://127.0.0.1:5000/v1/asus/zenfone").json()
     return r["zenfone"]
 
-name = get_zenfone()["zenfone-8-flip"]
-print(name)
+args = sys.argv[1:][0]
+name = get_zenfone()[f"{args}"]
 
 model = name["model"]
 download_url = name["download_url"]
 print(model)
 print(download_url)
 
-def download_device(model: str, download_model_name: str):
-    r = get(
-        f"https://dlcdnets.asus.com/pub/ASUS/ZenFone/{model}/{download_model_name}"
-    )
-    print(r.text)
-
-    with open(download_url, 'wb') as file:
-        file.write(r.content)
+def download_device(model: str, d: str):
+    file = f"https://dlcdnets.asus.com/pub/ASUS/ZenFone/{model}/{d}"
+    system(f"wget {file}")
 
 get_download = download_device(
     model=model,
-    download_model_name=download_url
+    d=download_url
 )
 
-print(get_download)
